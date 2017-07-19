927168e3a6f6ef8fde4aca0cf1abe4d15e98521f
Fynn <fynnfluegge@gmx.de>
2017-07-19 20:12:57 +0200

    final commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Lwjgl3-Game-Engine-StartingCode
-This is the Project Starting Code Template for the Lwjgl 3 Game Engine Programming Series
+##This is the Project Starting Code Template for the Lwjgl 3 Game Engine Programming Series
+
+##Template implements:
+* GLFW Window
+* GLFW Inputhandler
+* Game Loop
+* Camera
+* 3D Math (Matrices,Vectors)
+* OBJ-File Loader
+* GLSL-File Loader
--- oreon.engine/src/core/utils/ResourceLoader.java
+++ oreon.engine/src/core/utils/ResourceLoader.java
@@ -1,5 +1,32 @@
 package core.utils;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class ResourceLoader {
 
+	public static String loadShader(String fileName)
+	{
+		StringBuilder shaderSource = new StringBuilder();
+		BufferedReader shaderReader = null;
+		
+		try
+		{
+			shaderReader = new BufferedReader(new FileReader("./res/" + fileName));
+			String line;
+			while((line = shaderReader.readLine()) != null)
+			{
+				shaderSource.append(line).append("\n");
+			}
+			
+			shaderReader.close();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		return shaderSource.toString();
+	}
 }
