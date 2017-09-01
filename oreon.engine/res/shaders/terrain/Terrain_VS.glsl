e60eae797547f8f51294565771266aee2db00592
Fynn <fynnfluegge@gmx.de>
2017-09-01 18:09:29 +0200

    tutorial 2.2

--- oreon.engine/res/shaders/terrain/Terrain_VS.glsl
+++ oreon.engine/res/shaders/terrain/Terrain_VS.glsl
@@ -2,12 +2,121 @@
 
 layout (location = 0) in vec2 position0;
 
+uniform vec3 cameraPosition;
 uniform mat4 localMatrix;
 uniform mat4 worldMatrix;
+uniform float scaleY;
+uniform int lod;
+uniform vec2 index;
+uniform float gap;
+uniform vec2 location;
+uniform int lod_morph_area[8];
 
-void main()
-{
-	vec2 localPosition = (localMatrix * vec4(position0.x,0,position0.y,1)).xz;
+float morphLatitude(vec2 position) {
+	
+	vec2 frac = position - location;
+	
+	if (index == vec2(0,0)){
+		float morph = frac.x - frac.y;
+		if (morph > 0)
+			return morph;
+	}
+	if (index == vec2(1,0)){
+		float morph = gap - frac.x - frac.y;
+		if (morph > 0)
+			return morph;
+	}
+	if (index == vec2(0,1)){
+		float morph = frac.x + frac.y - gap;
+		if (morph > 0)
+			return -morph;
+	}
+	if (index == vec2(1,1)){
+		float morph = frac.y - frac.x;
+		if (morph > 0)
+			return -morph;
+	}
+	return 0;
+}
+
+float morphLongitude(vec2 position) {
+	
+	vec2 frac = position - location;
+	
+	if (index == vec2(0,0)){
+		float morph = frac.y - frac.x;
+		if (morph > 0)
+			return -morph;
+	}
+	if (index == vec2(1,0)){
+		float morph = frac.y - (gap - frac.x);
+		if (morph > 0)
+			return morph;
+	}
+	if (index == vec2(0,1)){
+		float morph = gap - frac.y - frac.x;
+		if (morph > 0)
+			return -morph;
+	}
+	if (index == vec2(1,1)){
+		float morph = frac.x - frac.y;
+		if (morph > 0)
+			return morph;
+	}
+	return 0;
+}
+
+vec2 morph(vec2 localPosition, int morph_area){
+	
+	vec2 morphing = vec2(0,0);
+	
+	vec2 fixPointLatitude = vec2(0,0);
+	vec2 fixPointLongitude = vec2(0,0);
+	float distLatitude;
+	float distLongitude;
+	
+	if (index == vec2(0,0)) {
+		fixPointLatitude = location + vec2(gap,0);
+		fixPointLongitude = location + vec2(0,gap);
+	}
+	else if (index == vec2(1,0)) {
+		fixPointLatitude = location;
+		fixPointLongitude = location + vec2(gap,gap);
+	}
+	else if (index == vec2(0,1)) {
+		fixPointLatitude = location + vec2(gap,gap);
+		fixPointLongitude = location;
+	}
+	else if (index == vec2(1,1)) {
+		fixPointLatitude = location + vec2(0,gap);
+		fixPointLongitude = location + vec2(gap,0);
+	}
+	
+	float planarFactor = 0;
+	if (cameraPosition.y > abs(scaleY))
+		planarFactor = 1;
+	else
+		planarFactor = cameraPosition.y/ abs(scaleY);
+		
+	distLatitude = length(cameraPosition - (worldMatrix * 
+					vec4(fixPointLatitude.x,planarFactor,fixPointLatitude.y,1)).xyz);
+	distLongitude = length(cameraPosition - (worldMatrix * 
+					vec4(fixPointLongitude.x,planarFactor,fixPointLongitude.y,1)).xyz);
 					
+	if (distLatitude > morph_area)
+		morphing.x = morphLatitude(localPosition.xy);
+	if (distLongitude > morph_area)
+		morphing.y = morphLongitude(localPosition.xy);
+		
+	return morphing;
+}
+
+void main(){
+
+	vec2 localPosition = (localMatrix * vec4(position0.x,0,position0.y,1)).xz;
+	
+	if (lod > 0)
+		localPosition += morph(localPosition,lod_morph_area[lod-1]);
+	
 	gl_Position = worldMatrix * vec4(localPosition.x,0,localPosition.y,1);
 }
--- oreon.engine/src/modules/terrain/TerrainShader.java
+++ oreon.engine/src/modules/terrain/TerrainShader.java
@@ -6,40 +6,65 @@ import core.shaders.Shader;
 import core.utils.ResourceLoader;
 
 public class TerrainShader extends Shader{
-
-private static TerrainShader instance = null;
 	
-	public static TerrainShader getInstance() 
-	{
-	    if(instance == null) 
-	    {
-	    	instance = new TerrainShader();
-	    }
-	      return instance;
+	private static TerrainShader instance = null;
+	
+	public static TerrainShader getInstance(){
+		
+		if (instance == null){
+			
+			instance = new TerrainShader();
+		}
+		return instance;
 	}
 	
-	protected TerrainShader()
-	{
+	protected TerrainShader(){
+		
 		super();
-
-		addVertexShader(ResourceLoader.loadShader("shaders/terrain/Terrain_VS.glsl"));
-		addTessellationControlShader(ResourceLoader.loadShader("shaders/terrain/Terrain_TC.glsl"));
-		addTessellationEvaluationShader(ResourceLoader.loadShader("shaders/terrain/Terrain_TE.glsl"));
-		addGeometryShader(ResourceLoader.loadShader("shaders/terrain/Terrain_GS.glsl"));
-		addFragmentShader(ResourceLoader.loadShader("shaders/terrain/Terrain_FS.glsl"));
+		
+		addVertexShader(ResourceLoader.loadShader("shaders/terrain/terrain_VS.glsl"));
+		addTessellationControlShader(ResourceLoader.loadShader("shaders/terrain/terrain_TC.glsl"));
+		addTessellationEvaluationShader(ResourceLoader.loadShader("shaders/terrain/terrain_TE.glsl"));
+		addGeometryShader(ResourceLoader.loadShader("shaders/terrain/terrain_GS.glsl"));
+		addFragmentShader(ResourceLoader.loadShader("shaders/terrain/terrain_FS.glsl"));
+		
 		compileShader();
-	
+		
 		addUniform("localMatrix");
 		addUniform("worldMatrix");
-
 		addUniform("m_ViewProjection");
+		
+		addUniform("index");
+		addUniform("gap");
+		addUniform("lod");
+		addUniform("scaleY");
+		addUniform("location");
+		addUniform("cameraPosition");
+		
+		for (int i=0; i<8; i++){
+			addUniform("lod_morph_area[" + i + "]");
+		}
 	}
 	
-	public void updateUniforms(GameObject object)
-	{	
-		setUniform("m_ViewProjection", Camera.getInstance().getViewProjectionMatrix());
+	public void updateUniforms(GameObject object) {
 		
+		TerrainNode terrainNode = (TerrainNode) object;
+		
+		setUniformf("scaleY", terrainNode.getConfig().getScaleY());
+		setUniformi("lod", terrainNode.getLod());
+		setUniform("index", terrainNode.getIndex());
+		setUniform("location", terrainNode.getLocation());
+		setUniformf("gap", terrainNode.getGap());
+		
+		for (int i = 0; i<8; i++){
+			setUniformi("lod_morph_area[" + i + "]", terrainNode.getConfig().getLod_morphing_area()[i]);
+		}
+		
+		setUniform("cameraPosition", Camera.getInstance().getPosition());
+		setUniform("m_ViewProjection", Camera.getInstance().getViewProjectionMatrix());
 		setUniform("localMatrix", object.getLocalTransform().getWorldMatrix());
 		setUniform("worldMatrix", object.getWorldTransform().getWorldMatrix());
 	}
+
 }
+
